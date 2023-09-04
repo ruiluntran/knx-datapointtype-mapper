752fa11c3f8913a4d17db28389d84e451d27ee7c
Rui Lun Tran <ruilun.tran@gmail.com>
2023-09-04 15:07:28 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # KNX ETS Group Addresses Datapoint Type Mapper
+
+When exporting the group addresses from ETS, the Datapoint type are declared like
+DPST-1-1. If you want to use 
+
+
+DPST-10-1
+DPST-19-1
+DPST-14-19
+
+DPST-1-1
+DPST-1-1
+DPST-1-1
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-function knxDatapointTypeMapper(dataPoint) {
-  const partial = dataPoint.split('-');
-  return 'DPT' + partial[1] + '.' + ('000' + partial[2]).substr(-3);
-}
+'use strict';
 
-export {
-  knxDatapointTypeMapper
+module.exports = {
+  knxDatapointTypeMapper: (dataPoint) => {
+
+    const partial = dataPoint.split('-');
+
+    if (partial.length === 2) {
+      partial[2] = 1;
+    }
+
+    return 'DPT' + partial[1] + '.' + ('000' + partial[2]).substr(-3);
+  }
 }
